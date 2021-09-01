410f0a6cc78b1e3a1b1e50fffbf7e9a8018ebae8
hungnn88dn@gmail.com <hungnn88dn@gmail.com>
2021-09-01 16:16:29 +0700

    hungnn

---
+++ Module_2/src/Thuc_Hanh/Bai01/system_time.java
@@ -0,0 +1,10 @@
+package Thuc_Hanh.Bai01;
+
+import java.util.Date;
+
+public class system_time {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is :" + now);
+    }
+}
