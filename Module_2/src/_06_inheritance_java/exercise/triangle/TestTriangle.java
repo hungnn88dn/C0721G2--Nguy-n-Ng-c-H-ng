8e16bbc36857e2fb30873ffd26d807844abb691d
hungnn88dn@gmail.com <hungnn88dn@gmail.com>
2021-09-09 15:49:36 +0700

    hungnn

---
+++ Module_2/src/_06_inheritance_java/exercise/triangle/TestTriangle.java
@@ -0,0 +1,17 @@
+package _06_inheritance_java.exercise.triangle;
+import java.awt.geom.CubicCurve2D;
+import java.util.Scanner;
+public class TestTriangle {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Nhập độ dài 3 cạnh :");
+        double side1= Double.parseDouble(scanner.nextLine());
+        double side2= Double.parseDouble(scanner.nextLine());
+        double side3= Double.parseDouble(scanner.nextLine());
+        System.out.println("Nhập màu của tam giác: ");
+        String color= scanner.nextLine();
+        Triangle triangle= new Triangle(color,true,side1,side2,side3);
+        System.out.println("Màu sắc của tam giác là: " + color + ", chu vi của tam giác là : " + triangle.getPerimeter() + ", diện tích của tam giác là : " + triangle.getArea());
+
+    }
+}
