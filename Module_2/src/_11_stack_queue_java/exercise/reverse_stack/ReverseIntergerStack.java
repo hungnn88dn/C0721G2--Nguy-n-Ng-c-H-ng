7890ecc00c6129a105ffcd9f782dc4bd33245d50
hungnn88dn@gmail.com <hungnn88dn@gmail.com>
2021-09-15 17:23:56 +0700

    hungnn

---
+++ Module_2/src/_11_stack_queue_java/exercise/reverse_stack/ReverseIntergerStack.java
@@ -0,0 +1,30 @@
+package _11_stack_queue_java.exercise.reverse_stack;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReverseIntergerStack {
+    public static void main(String[] args) {
+        Stack<Integer> myStack= new Stack<>();
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Nhập số lượng phần tử trong mảng: ");
+        int number= scanner.nextInt();
+        int[] array= new int[number];
+        for( int i=0; i < number; i++) {
+            System.out.println("Nhập giá trị phần tử trong mảng");
+            array[i]= scanner.nextInt();
+        }
+        System.out.println("Giá trị của mảng trước khi đảo ");
+        System.out.println(Arrays.toString( array));
+        for( int i=0; i < array.length; i++) {
+            myStack.push(array[i]);
+        }
+        for( int i=0; i < array.length; i++) {
+            array[i] = myStack.get(array.length -i - 1);
+        }
+        System.out.println("---------");
+        System.out.println("Giá trị của mảng sau khi đảo ");
+        System.out.println(Arrays.toString(array));
+    }
+}
---
+++ Module_2/src/_11_stack_queue_java/exercise/string/map/StringMap.java
@@ -0,0 +1,26 @@
+package _11_stack_queue_java.exercise.string.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class StringMap {
+    public static void main(String[] args) {
+        Map<Character, Integer> myMap = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập chuỗi cần kiểm tra");
+        String str = scanner.nextLine();
+        int count = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (myMap.containsKey(str.charAt(i))) {
+                myMap.replace(str.charAt(i),myMap.get(str.charAt(i)) + 1 );
+            } else {
+                myMap.put(str.charAt(i), 1);
+            }
+        }
+        System.out.println(myMap);
+    }
+
+
+}
