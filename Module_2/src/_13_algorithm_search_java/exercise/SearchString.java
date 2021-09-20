902a13b731e5d1e228a470edc7148ccd06e07bda
hungnn88dn@gmail.com <hungnn88dn@gmail.com>
2021-09-20 18:16:23 +0700

    hungnn

--- Module_2/src/_13_algorithm_search_java/exercise/SearchString.java
+++ Module_2/src/_13_algorithm_search_java/exercise/SearchString.java
@@ -1,8 +1,26 @@
 package _13_algorithm_search_java.exercise;
 
-import java.util.Scanner;
+
+import java.util.*;
 
 public class SearchString {
-    Scanner scanner= new Scanner(System.in);
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập chuỗi cần sắp xếp");
+        String str = scanner.nextLine();
+        List<String> listStr = new ArrayList<>();
+        listStr.add(Character.toString(str.charAt(0)));
+        for (int i = 1; i < str.length(); i++) {
+                if (Character.toString(str.charAt(i)).compareTo(listStr.get(listStr.size() - 1)) > 0) {
+                    listStr.add(Character.toString(str.charAt(i)));
+                }
+        }
+        System.out.println(listStr);
+    }
 }
+//  Độ phức tạp của bài toán:
+//    1 vòng lặp = n ;
+//    1 câu lệnh = 1;
+//    T(n) = n + 1;
+//    T(n)= O(n);
+//    ==> Độ phực tạp là O(n);
