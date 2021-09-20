777862efc413e5722d2155cd2fd2b765b401fddd
hungnn88dn@gmail.com <hungnn88dn@gmail.com>
2021-09-20 19:04:34 +0700

    hungnn

--- Module_2/src/_13_algorithm_search_java/exercise/SearchString.java
+++ Module_2/src/_13_algorithm_search_java/exercise/SearchString.java
@@ -8,19 +8,29 @@ public class SearchString {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập chuỗi cần sắp xếp");
         String str = scanner.nextLine();
-        List<String> listStr = new ArrayList<>();
-        listStr.add(Character.toString(str.charAt(0)));
-        for (int i = 1; i < str.length(); i++) {
-                if (Character.toString(str.charAt(i)).compareTo(listStr.get(listStr.size() - 1)) > 0) {
-                    listStr.add(Character.toString(str.charAt(i)));
+        List<String> listMax = new ArrayList<>();
+        listMax.add(Character.toString(str.charAt(0)));
+        for (int i = 0; i < str.length(); i++) {
+            List<String> listTemp = new ArrayList<>();
+            listTemp.add(Character.toString(str.charAt(i)));
+            for (int j = i + 1; j < str.length(); j++) {
+                if (Character.toString(str.charAt(j)).compareTo(listTemp.get(listTemp.size() - 1)) > 0) {
+                    listTemp.add(Character.toString(str.charAt(j)));
                 }
+            }
+            if (listTemp.size() > listMax.size()) {
+                listMax.clear();
+                listMax.addAll(listTemp);
+            }
+            listTemp.clear();
         }
-        System.out.println(listStr);
+        System.out.println(listMax);
     }
 }
 //  Độ phức tạp của bài toán:
-//    1 vòng lặp = n ;
+//    1 vòng lặp ngoài = m ;
+//    1 vòng lặp trong = n ;
 //    1 câu lệnh = 1;
-//    T(n) = n + 1;
-//    T(n)= O(n);
-//    ==> Độ phực tạp là O(n);
+//    T(n) = (m * n) + 1;
+//    T(n)= O(mn);
+//    ==> Độ phực tạp là O(mn);
