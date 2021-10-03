708c90c1987266ba8c87141a381754b334d97ed8
hungnn88dn@gmail.com <hungnn88dn@gmail.com>
2021-10-03 12:06:35 +0700

    hungnn

--- FuramaResort/src/controllers/Test.java
+++ FuramaResort/src/controllers/Test.java
@@ -14,8 +14,5 @@ public class Test {
     public static void main(String[] args) {
         FuramaController furamaController= new FuramaController();
         furamaController.displayMainMenu();
-
-
-
     }
 }
