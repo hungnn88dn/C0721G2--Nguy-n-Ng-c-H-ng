f9a3921defe1bb93319cf36180be66db39cd7592
hungnn88dn@gmail.com <hungnn88dn@gmail.com>
2021-10-18 16:10:45 +0700

    hungnn

--- Module3/_05_SQLView_ Index _Stored Procedure/thuc_hanh/db_index_view_produce_all.sql
+++ Module3/_05_SQLView_ Index _Stored Procedure/thuc_hanh/db_index_view_produce_all.sql
@@ -7929,6 +7929,12 @@ insert  into `products`(`productCode`,`productName`,`productLine`,`productScale`
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+
+
+-------------------------------------------------------------------------------------------------------------------------------
+
+-- [Thực hành] Chỉ mục trong MySql
+
 SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.';
 
 EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.'; 
@@ -7942,6 +7948,12 @@ ALTER TABLE customers ADD INDEX idx_full_name(contactFirstName, contactLastName)
 EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
 
 ALTER TABLE customers DROP INDEX idx_full_name;
+
+
+
+
+----------------------------------------------------------------------------------------------------------------------------------
+-- [Thực hành] Store Procedure
 
 DELIMITER //
 
@@ -7957,6 +7969,9 @@ DELIMITER ;
 
 call findAllCustomers();
 
+
+
+
 DELIMITER //
 DROP PROCEDURE IF EXISTS `findAllCustomers`//
 
@@ -7967,3 +7982,104 @@ BEGIN
 SELECT * FROM customers where customerNumber = 175;
 
 END //
+
+DELIMITER //
+
+-- ---------------------------------------------------------------------------------------------------------------------------------------------------------
+-- -- [Thực hành] Truyền tham số vào Store Procedure
+
+CREATE PROCEDURE getCusById
+
+(IN cusNum INT(11))
+
+BEGIN
+
+  SELECT * FROM customers WHERE customerNumber = cusNum;
+
+END //
+
+DELIMITER ;
+
+call getCusById(175);
+
+DELIMITER //
+
+CREATE PROCEDURE GetCustomersCountByCity(
+
+    IN  in_city VARCHAR(50),
+
+    OUT total INT
+
+)
+
+BEGIN
+
+    SELECT COUNT(customerNumber)
+
+    INTO total
+
+    FROM customers
+
+    WHERE city = in_city;
+
+END//
+
+DELIMITER ;
+
+CALL GetCustomersCountByCity('Lyon',@total);
+
+SELECT @total;
+
+DELIMITER //
+
+CREATE PROCEDURE SetCounter(
+
+    INOUT counter INT,
+
+    IN inc INT
+
+)
+
+BEGIN
+
+    SET counter = counter + inc;
+
+END//
+
+DELIMITER ;
+
+SET @counter = 1;
+
+CALL SetCounter(@counter,1); -- 2
+
+CALL SetCounter(@counter,1); -- 3
+
+CALL SetCounter(@counter,5); -- 8
+
+SELECT @counter; -- 8
+
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- [Thực hành] View trong MySql
+
+CREATE VIEW customer_views AS
+
+SELECT customerNumber, customerName, phone
+
+FROM  customers;
+
+select * from customer_views;
+
+CREATE OR REPLACE VIEW customer_views AS
+
+SELECT customerNumber, customerName, contactFirstName, contactLastName, phone
+
+FROM customers
+
+WHERE city = 'Nantes';
+
+DROP VIEW view_name;
+
+DROP VIEW customer_views;
+
+
